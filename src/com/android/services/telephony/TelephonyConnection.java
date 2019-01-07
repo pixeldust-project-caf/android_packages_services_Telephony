46354cbd7f6f2eb77b0cc603f6631c212a34c8d4
Ping Sun <ping.x.sun@sony.com>
2019-01-07 10:01:49 +0800

    Fix the drawable resource can't be loaded from RRO package
    Since refer to resources from the overlay package using the IDs defined
    in the original package, NotFoundException occurs and icons can NOT be
    loaded.
    These are caused by using Icon.createWithResource(Resources, ResId),
    and usage of this API is NOT supported.
    
    To resolve this issue,  use Icon.createWithResource(Context, ResId) for
    create icons with original package.
    
    Test: manual - Check that SIP call account icon is displayed with the
    specified in the overlay package
    Bug: 124352738
    
    Change-Id: I43be48818b190698a4344d8fc8f2ac5a6927702a
    (cherry picked from commit e26359185bb4866ba93507c5b3d6791a1e8880cb)
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -2222,8 +2222,7 @@ abstract class TelephonyConnection extends Connection implements Holdable,
             setStatusHints(new StatusHints(
                     context.getString(labelId) + displaySubId,
                     Icon.createWithResource(
-                            context.getResources(),
-                            R.drawable.ic_signal_wifi_4_bar_24dp),
+                            context, R.drawable.ic_signal_wifi_4_bar_24dp),
                     null /* extras */));
         } else {
             setStatusHints(null);
