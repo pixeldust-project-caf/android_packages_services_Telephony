71473da34ede0b266b3aad11184eb8c1fe8621f5
Ricardo Cerqueira <ricardo@cyngn.com>
2019-09-27 23:00:18 -0400

    Break MSIM dependency on qti-telephony-framework
    If the provisioning state is invalid, the framework is most likely
    absent. Consider the card as provisioned to pass all relevant checks.
    
    Change-Id: Iebce33b4fd49632f404341ddb2022282316f4f50

--- src/com/android/services/telephony/TelecomAccountRegistry.java
+++ src/com/android/services/telephony/TelecomAccountRegistry.java
@@ -1178,7 +1178,6 @@ public class TelecomAccountRegistry {
                     // states we are interested in from what
                     // IExtTelephony.getCurrentUiccCardProvisioningStatus()can return
                     final int PROVISIONED = 1;
-                    final int INVALID_STATE = -1;
                     final int CARD_NOT_PRESENT = -2;
                     boolean isInEcm = false;
 
@@ -1198,11 +1197,9 @@ public class TelecomAccountRegistry {
                                 provisionStatus =
                                         mExtTelephony.getCurrentUiccCardProvisioningStatus(slotId);
                             } catch (RemoteException ex) {
-                                provisionStatus = INVALID_STATE;
                                 Log.w(this, "Failed to get status , slotId: "+ slotId +" Exception: "
                                         + ex);
                             } catch (NullPointerException ex) {
-                                provisionStatus = INVALID_STATE;
                                 Log.w(this, "Failed to get status , slotId: "+ slotId +" Exception: "
                                         + ex);
                             }
@@ -1210,7 +1207,7 @@ public class TelecomAccountRegistry {
 
                         // In SSR case, UiccCard's would be disposed hence the provision state received as
                         // CARD_NOT_PRESENT but valid subId present in SubscriptionInfo record.
-                        if (provisionStatus == INVALID_STATE || ((provisionStatus == CARD_NOT_PRESENT)
+                        if (((provisionStatus == CARD_NOT_PRESENT)
                                 && mSubscriptionManager.isActiveSubId(subscriptionId))) {
                             isAnyProvisionInfoPending = true;
                         }
