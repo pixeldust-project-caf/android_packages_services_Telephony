ce842e87ddcfc080136f23b85730c9f80f67e3bd
Ganesh Vilas Chaudhari <quic_ganeshvi@quicinc.com>
2022-09-29 14:43:05 +0530

    Use data registration state to show network selection notification
    -For SNPN mode, Show network selection notification based on data registration state.
    
    Change-Id: I56eb27d0484969ec19f6c69acce815b6a5a96288
    CRs-Fixed: 3288701

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -84,6 +84,8 @@ import com.android.phone.settings.SettingsConstants;
 import com.android.phone.vvm.CarrierVvmPackageInstalledReceiver;
 import com.android.services.telephony.rcs.TelephonyRcsService;
 
+import com.qti.extphone.ExtTelephonyManager;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.lang.annotation.Retention;
@@ -156,6 +158,7 @@ public class PhoneGlobals extends ContextWrapper {
         FULL
     }
 
+    private static final String NETWORK_ACCESS_MODE = "access_mode";
     private static PhoneGlobals sMe;
 
     CallManager mCM;
@@ -972,7 +975,7 @@ public class PhoneGlobals extends ContextWrapper {
 
     private void handleServiceStateChanged(ServiceState serviceState, int subId) {
         if (VDBG) Log.v(LOG_TAG, "handleServiceStateChanged");
-        int state = serviceState.getState();
+        int state = getRegistrationState(serviceState, subId);
         notificationMgr.updateNetworkSelection(state, subId);
 
         if (VDBG) {
@@ -982,6 +985,17 @@ public class PhoneGlobals extends ContextWrapper {
         if (subId == mDefaultDataSubId) {
             updateDataRoamingStatus();
         }
+    }
+
+    private int getRegistrationState(ServiceState serviceState, int subId) {
+        int state = serviceState.getState();
+        int accessMode = Settings.Global.getInt(getContentResolver(),
+                NETWORK_ACCESS_MODE + SubscriptionManager.getSlotIndex(subId),
+                ExtTelephonyManager.ACCESS_MODE_PLMN);
+        if (accessMode == ExtTelephonyManager.ACCESS_MODE_SNPN) {
+            state = serviceState.getDataRegState();
+        }
+        return state;
     }
 
     /**
@@ -1125,7 +1139,8 @@ public class PhoneGlobals extends ContextWrapper {
     public void onNetworkSelectionChanged(int subId) {
         Phone phone = getPhone(subId);
         if (phone != null) {
-            notificationMgr.updateNetworkSelection(phone.getServiceState().getState(), subId);
+            int state = getRegistrationState(phone.getServiceState(), subId);
+            notificationMgr.updateNetworkSelection(state, subId);
         } else {
             Log.w(LOG_TAG, "onNetworkSelectionChanged on null phone, subId: " + subId);
         }
